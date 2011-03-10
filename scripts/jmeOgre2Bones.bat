62fe6ea23abbbb8a170b6eed26007f4007d2d149
Hakan Eryargi <hakan.eryargi@gmail.com>
2011-03-10 10:58:33 +0000

    switched to jME 2.1

--- scripts/jmeOgre2Bones.bat
+++ scripts/jmeOgre2Bones.bat
@@ -2,10 +2,10 @@ echo off
 set DIR=%~dp0\..
 
 set CP=%DIR%\build\bones.jar
-set CP=%CP%;%DIR%\lib\jpct-1.21.alpha.jar
+set CP=%CP%;%DIR%\lib\jpct-1.22.jar
 
-set CP=%CP%;%DIR%\lib\jme-2.0.1.jar
-set CP=%CP%;%DIR%\lib\jme-model-2.0.1.jar
-set CP=%CP%;%DIR%\lib\jme-ogrexml-2.0.1-exposed.jar
+set CP=%CP%;%DIR%\lib\jme-2.1.jar
+set CP=%CP%;%DIR%\lib\jme-model-2.1.jar
+set CP=%CP%;%DIR%\lib\jme-ogrexml-2.1.jar
 
 java -cp %CP% raft.jpct.bones.util.JMEOgreImporter %1 %2 %3 %4 %5 %6 %7 %8 %9
